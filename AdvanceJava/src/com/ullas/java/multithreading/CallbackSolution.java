16a1718d200c80b78034dad8e063e146072264eb
ullasnaik <naik.ullas@ymail.com>
2017-05-12 15:39:55 +0530

    zd

---
+++ AdvanceJava/src/com/ullas/java/multithreading/CallbackSolution.java
@@ -0,0 +1,32 @@
+package com.ullas.java.multithreading;
+
+public class CallbackSolution {
+
+	public static void main(String[] args) {
+		new MyThread.start();
+	}
+
+	public void mymeth(String str) {
+		System.out.println(str);
+	}
+
+}
+
+class MyThread extends Thread {
+
+	Callback callback;
+
+	public MyThread(Callback callback) {
+		this.callback = callback;
+	}
+
+	public void run() {
+		callback.mymeth("Hello");
+	}
+
+
+
+}
+ interface Callback {
+	int mymeth(String s);
+}
