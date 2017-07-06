487df50dcd7421661fa20dc6092cfd72ed8a5808
ullasnaik <naik.ullas@ymail.com>
2017-07-06 12:32:33 +0530

    Java 8 Stream

---
+++ AdvanceJava/src/com/ullas/java8/StreamBuilders.java
@@ -0,0 +1,21 @@
+package com.ullas.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamBuilders {
+    public static void main(String[] args){
+    	List<String> memberNames = new ArrayList<>();
+    	memberNames.add("Amitabh");
+    	memberNames.add("Shekhar");
+    	memberNames.add("Aman");
+    	memberNames.add("Rahul");
+    	memberNames.add("Shahrukh");
+    	memberNames.add("Salman");
+    	memberNames.add("Yana");
+    	memberNames.add("Lokesh");
+    	memberNames.stream().filter((a) -> a.startsWith("A"))
+    	.map(String::toUpperCase)
+        .forEach(System.out::println);
+    }
+}
